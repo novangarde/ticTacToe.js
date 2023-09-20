c92d9448bc225ad8427389b35aa8931d956808fc
VlasBabinets <moostanger@gmail.com>
2023-09-20 15:14:30 +0500

    First version is completed

---
+++ js.js
@@ -0,0 +1,98 @@
+"use strict";
+
+const ticTacToe = {
+  gameTableElement: document.getElementById("game"),
+  status: "playing",
+  mapValues: [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ],
+  phase: "X",
+
+  init() {
+    this.renderMap();
+    this.initEventHandlers();
+  },
+
+  renderMap() {
+    for (let row = 0; row < 3; row++) {
+      const tr = document.createElement("tr");
+      this.gameTableElement.appendChild(tr);
+      for (let col = 0; col < 3; col++) {
+        const td = document.createElement("td");
+
+        td.dataset.row = row; // в новый тег td добавляется аттрибут data-row с номером строки
+        td.dataset.col = col; // в новый тег td добавляется аттрибут data-col с номером колонки
+
+        tr.appendChild(td);
+      }
+    }
+  },
+
+  initEventHandlers() {
+    this.gameTableElement.addEventListener("click", (event) =>
+      this.cellClickHandler(event)
+    );
+  },
+
+  cellClickHandler(event) {
+    const row = event.target.dataset.row;
+    const col = event.target.dataset.col;
+
+    if (!this.isClickCorrect(event)) {
+      return;
+    }
+
+    this.mapValues[row][col] = this.phase;
+    event.target.textContent = this.phase;
+
+    if (this.hasWon()) {
+      this.status = "stopped";
+      alert(`Победили ${this.phase}-ки`);
+    }
+
+    if (this.phase === "X") {
+      this.phase = "0";
+    } else {
+      this.phase = "X";
+    }
+  },
+
+  isClickCorrect(event) {
+    return this.isCellEmpty(event) && this.isStatusPlaying();
+  },
+
+  isCellEmpty(event) {
+    const row = event.target.dataset.row;
+    const col = event.target.dataset.col;
+    return this.mapValues[row][col] === "";
+  },
+
+  isStatusPlaying() {
+    return this.status === "playing";
+  },
+
+  hasWon() {
+    return (
+      this.isLineWon({ x: 0, y: 0 }, { x: 1, y: 0 }, { x: 2, y: 0 }) ||
+      this.isLineWon({ x: 0, y: 1 }, { x: 1, y: 1 }, { x: 2, y: 1 }) ||
+      this.isLineWon({ x: 0, y: 2 }, { x: 1, y: 2 }, { x: 2, y: 2 }) ||
+
+      this.isLineWon({ x: 0, y: 0 }, { x: 0, y: 1 }, { x: 0, y: 2 }) ||
+      this.isLineWon({ x: 1, y: 0 }, { x: 1, y: 1 }, { x: 1, y: 2 }) ||
+      this.isLineWon({ x: 2, y: 0 }, { x: 2, y: 1 }, { x: 2, y: 2 }) ||
+
+      this.isLineWon({ x: 0, y: 0 }, { x: 1, y: 1 }, { x: 2, y: 2 }) ||
+      this.isLineWon({ x: 0, y: 2 }, { x: 1, y: 1 }, { x: 2, y: 0 })
+    );
+  },
+
+  isLineWon(a, b, c) {
+    const value =
+      this.mapValues[a.y][a.x] +
+      this.mapValues[b.y][b.x] +
+      this.mapValues[c.y][c.x];
+    return value === "XXX" || value === "000";
+  },
+};
